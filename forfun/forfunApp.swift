1963525f76179970cef575ac2b08e6364838c594
Loujain <landejany@flyakeed.com>
2024-05-16 21:32:59 +0300

    Initial Commit

---
+++ forfun/forfunApp.swift
@@ -0,0 +1,17 @@
+//
+//  forfunApp.swift
+//  forfun
+//
+//  Created by JAY on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct forfunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
